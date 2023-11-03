1681617ce4470882c65a1e2097d01727e503ad60
Hisham Elmorsi <hmelmorsi@gmail.com>
2023-11-04 01:26:53 +0200

    Handling localization and getting jobs with other fields required

--- crawler.js
+++ crawler.js
@@ -1,45 +1,128 @@
 const cheerio = require('cheerio')
 const {gotScraping} = require('got-scraping')
 
-const crawl = async()=>{
 
-    const websiteUrl = 'https://www.linkedin.com'
-    const jobSearchUrl = `${websiteUrl}/jobs/search`
-    const filterUrl = `${jobSearchUrl}?keywords=Back%20End%20Developer&location=Egypt&locationId=&geoId=106155005&f_TPR=r86400&position=1&pageNum=0`
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 
-    const response = await gotScraping(filterUrl)
-    const html = response.body
 
-    const $ = cheerio.load(html)
+const country = 'Egypt'
+const websiteUrl = 'https://www.linkedin.com'
+const jobSearchUrl = `${websiteUrl}/jobs/search`
+const filterJobsUrl = `${jobSearchUrl}?location=Egypt&geoId=106155005&locationId=&keywords=Back%20End%20Developer&f_TPR=r86400&countryRedirected=1&position=1&pageNum=0`
+
+
+const scrapeJobs = async()=>{
+
+    const jobsPageResponse = await gotScraping(filterJobsUrl)
+    const jobsPageHtml = jobsPageResponse.body    
+    const $ = cheerio.load(jobsPageHtml)
+
     // To get only a tags that has href attribut
     const links = $("a[class^='base-card']")
 
-    const jobUrls = []
-
+    const jobsUrls = []
     links.each((index, link) => {
         // using $ here turns the link into a selection object to be able to use cheerio methods on
         const url = $(link).attr('href')
-        // resolvedUrl is to make sure that all the links become absolute 
-        const resolvedUrl = new URL(url, websiteUrl).href
-        jobUrls.push(resolvedUrl)
+        // absoluteUrl is to make sure that all the links become absolute 
+        const absoluteUrl = new URL(url, websiteUrl).href
+        jobsUrls.push(absoluteUrl)
     })
 
-    for(const jobUrl of jobUrls){   
-        try {
-            const productResponse = await gotScraping(jobUrl);
-            const jobHtml = productResponse.body;
-            
-            $jobPage = cheerio.load(jobHtml);
-            const jobTitle = $jobPage('.top-card-layout__title').text();
-            console.log(jobTitle)
-            // catching the error will help to continue running the code and only printing the error
-        } catch(error){
-            console.log(error.message, jobUrl)
-        }
+    let {scrapedJobs, notScrapedJobs} = await scrapeFromUrls(jobsUrls)
+    let i = 0
 
+    while(i < 3 ) {
+
+        result = await scrapeFromUrls(notScrapedJobs)
+
+        scrapedJobs = [...scrapedJobs, ...result.scrapedJobs ]
+        notScrapedJobs = result.notScrapedJobs
+
+        i++;
+        
     }
+
+    console.log('Starting point URL for other urls', filterJobsUrl)
+    console.log('\n')
+    console.log('No.originalUrls', jobsUrls.length)
+    console.log('\n')
+    console.log(`No.notScraped = ${scrapedJobs.length}, No.Scraped = ${notScrapedJobs.length}`)
+    console.log('\n')
+    console.log(scrapedJobs)
 
 }
 
 
-crawl()
+const scrapeFromUrls= async (jobsUrls) => {
+
+    const notScrapedJobs = []
+    const scrapedJobs = []
+    for(const jobUrl of jobsUrls){   
+
+            const productResponse = await gotScraping(jobUrl);
+            const jobHtml = productResponse.body;
+            const $jobPage = cheerio.load(jobHtml);
+
+            try {
+
+                
+                const title = $jobPage('h1').text().trim();
+                // targets a elements with this class name . .....
+                const company = $jobPage('a.topcard__org-name-link').text().trim();
+                // .html returns null when non existing but text returns ''
+                const description = $jobPage('.description__text .show-more-less-html__markup').html().trim().split('\n').join('')
+                let level = $jobPage('ul.description__job-criteria-list').contents().eq(1).contents().eq(3).text().trim()
+
+                
+                if(level === 'مستوى المبتدئين'){
+                    level = 'junior'
+                } else if( level === 'مستوى متوسط الأقدمية'){
+                    level = 'mid-level'
+                }  else if(level === 'فترة تدريب'){
+                    level = 'intern'
+                } else if(level === 'مدير إداري'){
+                    level = 'lead'
+                } else if( level === 'غير مطبق'  || level === 'مساعد' || level === ''){
+                    level = 'other'
+                } 
+
+                let job = {
+
+                    title,
+                    company,
+                    country,
+                    description,
+                    level
+
+                }
+
+                
+
+                if (title == '' || company == '' || country == '' || description == '' || level == ''){
+                    throw new Error('Some field lost')
+                }
+                else {
+                    scrapedJobs.push(job)
+                }
+
+                await sleep(1000)
+    
+                // catching the error will help to continue running the code and only printing the error
+            } catch(error){
+                notScrapedJobs.push(jobUrl)
+
+            }
+
+    }
+
+
+    return { scrapedJobs, notScrapedJobs }
+
+}
+
+
+
+scrapeJobs()
