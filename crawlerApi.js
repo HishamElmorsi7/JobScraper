f7dbf9a91535ec3264ff710538a47f86cd407bd7
Hisham Elmorsi <hmelmorsi@gmail.com>
2023-12-09 16:03:49 +0200

    Adding for home page

--- crawlerApi.js
+++ crawlerApi.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json())
 
+app.get('/', (req, res) => {
+    res.end('Welcome to Job Scraperx')
+})
 app.get('/jobs', async (req, res) => {
     try {
         const jobsData = await crawl()
